01de1187ece9c17411e31345f4f850189aecb7f3
OUSSAMA OULOUARK <alioulouark@gmail.com>
2023-06-21 15:59:29 +0100

    Get Random Quote using StringRequest

---
+++ app/src/main/java/com/example/miniprojer02/StartActivity.java
@@ -0,0 +1,67 @@
+package com.example.miniprojer02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class StartActivity extends AppCompatActivity {
+
+    TextView tvStartActQuote, tvStartActAuthor;
+    Button btnStartActPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        tvStartActQuote = findViewById(R.id.tvStartActQuote);
+        tvStartActAuthor = findViewById(R.id.tvStartActAuthor);
+        btnStartActPass = findViewById(R.id.btnStartActPass);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://dummyjson.com/quotes/random ";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET, url,
+                response -> {
+                    // Display the first 500 characters of the response string.
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+
+                        tvStartActQuote.setText(jsonObject.getString("quote"));
+                        tvStartActAuthor.setText(jsonObject.getString("author"));
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        tvStartActQuote.setText("That didn't work!");
+                    }
+                });
+
+        stringRequest.setTag("TAG");
+        queue.add(stringRequest);
+
+        //endregion
+
+        btnStartActPass.setOnClickListener(v -> {
+            finish();
+        });
+    }
+    }
